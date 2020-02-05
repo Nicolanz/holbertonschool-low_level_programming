4938159146e337bd58cd651d14e248fd3f906847
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-05 22:22:56 +0000

    Intento 4.3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 	char string[60];
-	strcpy(string, "\"Programming is like building a multilingual puzzle\n");
+
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
 	puts(string);
 	return (0);
 }
