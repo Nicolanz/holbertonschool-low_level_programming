f4ae1cb5a05b436745f8f20cdd78d33a4e88a983
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-06 19:14:29 +0000

    Probar 1.3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,9 +27,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
 	}
-	else if (lastNum == 0)
+	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastNum);
+		printf("Last digit of %d is %d and is %d\n", n, lastNum, lastNum);
 	}
 	return (0);
 }
