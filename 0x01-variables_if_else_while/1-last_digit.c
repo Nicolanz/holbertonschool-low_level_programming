bd3aa8abfb6de15d67f2c0d59a98e6ec4d4682f1
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-07 03:56:43 +0000

    Probar 1.Algo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,24 +12,23 @@
 int main(void)
 {
 	int n;
-	int lastNum;
+	int numero;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	lastNum = n % 10;
+	numero = n % 10;
 
-	if (lastNum > 5)
+	if (numero > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
+		printf("Last digit of %d is %d and is greater than 5\n", n, numero);
 	}
-	else if (lastNum < 6)
+	else if (numero == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+		printf("Last digit of %d is %d and is 0\n", n, numero);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is %d\n", n, lastNum);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, numero);
 	}
-	return (0);
 }
