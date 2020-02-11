1b8487c533c9bf285b4c9e77e9d94de231cc14b8
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-11 19:17:04 +0000

    8/24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _abs - Print the absolute value of an int.
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int a;
+	int uno = 48;
+	int dos = 48;
+	int tres = 48;
+	int cuatro = 48;
+	for(a = 0; a < 1320; a++)
+	{
+		_putchar(cuatro);
+		_putchar(tres);
+		_putchar(58);
+		_putchar(dos);
+		_putchar(uno);
+		_putchar('\n');
+
+		if (uno == 57)
+		{
+			uno = 47;
+			dos++;
+		}
+		if (dos == 54 && uno == 47)
+		{
+			dos = 48;
+			tres++;
+		}
+		if (tres == 57 && dos == 48 && uno == 47)
+		{
+			tres = 48;
+			cuatro++;
+		}
+		uno++;
+	}
+}
