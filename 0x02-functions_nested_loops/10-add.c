1038fe721794163c1b297806507dd061aecc7626
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-11 22:18:22 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - function to add the values of numbers.
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
