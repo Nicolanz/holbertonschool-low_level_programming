5cdc88e866452820548cae3bc9f8744cdd023112
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-11 15:05:04 +0000

    Adding second exercise

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  *
  * Return: Always 0.
  */
