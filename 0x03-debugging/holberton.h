aac5780c40ca572696e3479a17cb25139bc65967
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-13 02:29:27 +0000

    three exercise

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int largest_number(int a, int b, int c);
-int positive_or_negative(int i);
+void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* HOLBERTON_H */
