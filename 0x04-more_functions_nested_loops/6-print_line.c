df735614fb9e2e6499b6b2ede06395d59bf49255
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-13 19:22:18 +0000

    Seis

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_line  - Print _ the number of lines specied.
+ * @n: Parameter to count the number of times thatn the i variable is repeated.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
