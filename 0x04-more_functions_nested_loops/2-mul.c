8f6800d1da8ab1e4f8f608ff7f9a153900b7c923
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-13 18:32:09 +0000

    Dos

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - Function to multiply two digits.
+ * @a: First parameter.
+ * @b: Second parameter.
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
