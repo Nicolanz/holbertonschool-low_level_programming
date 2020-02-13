8361d878f4e1bf7060a10ff625567962568dd6ba
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-13 19:00:37 +0000

    Cinco

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * more_numbers - Print the numbers from 0 to 14, 14 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 14; i++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (!(x < 10))
+			{
+				_putchar((x / 10) + '0');
+			}
+			_putchar((x % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
