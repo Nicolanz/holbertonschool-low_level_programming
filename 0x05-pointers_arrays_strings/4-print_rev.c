e9f41dd40cd48c3e278a20aac5601ee788d6480f
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-18 19:20:22 +0000

    Four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _puts_rev - print strings in reverse.
+ * @s: Variable which holds the string.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int cuenta;
+	int i;
+
+	while (s[cuenta] != 0)
+	{
+		cuenta++;
+	}
+	for (i = cuenta - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
