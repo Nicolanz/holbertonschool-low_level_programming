85961ff0641c10f4be6135e1aa4e7a13ced52790
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-18 14:53:49 +0000

    Zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - update the value to 98.
+ * @n: parameter to update.
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,7 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
 
+int _putchar(char c);
+void reset_to_98(int *n);
 
 #endif /* _SAMPLE_HEADER_H_ */
