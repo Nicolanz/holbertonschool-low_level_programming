cd8975fcee0dfacbe9c4e338ba517e6a1f806038
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-18 23:07:30 +0000

    Seis

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * puts2 - print strings in par position.
+ * @str: Variable which holds the string.
+ *
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int contador = 0;
+	int i;
+
+	while (str[contador] != '\0')
+	{
+		contador++;
+	}
+	for (i = 0; i <= contador; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
