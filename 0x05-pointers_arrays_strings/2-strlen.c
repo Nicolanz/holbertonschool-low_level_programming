edb9387fb5fdfff262d557626d0a19833cecad87
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-18 18:09:28 +0000

    Two

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - Prints the size of a string.
+ * @s: parameter to get the size.
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+
+	return (i);
+}
