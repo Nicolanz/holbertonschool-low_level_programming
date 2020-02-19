9265bba416c78b647d3723966ea36e919d0f2d40
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-19 03:36:59 +0000

    Nine

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strcpy - assign a sting to a declared variable.
+ * @dest: Variable which will hold the string.
+ * @src: Variable which holds the string.
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
