62483eb19b25389e86417196788a00d5c20a2ed9
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-20 16:05:32 +0000

    Concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Variable which will hold the string.
+ * @src: Variable which holds the other string.
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int contador = 0;
+
+	while (dest[contador] != '\0')
+	{
+		contador++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[contador] += src[i];
+		contador++;
+		i++;
+	}
+	return (dest);
+}
