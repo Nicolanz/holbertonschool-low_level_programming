3cc0080953280f5ff9811d941aa365726799091e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-20 21:06:35 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,13 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int total;
 
-	for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			total = s1[i] - s2[i];
-			return (total);
 			break;
+			return (total);
 		}
 	}
 	return (s1[i] - s2[i]);
