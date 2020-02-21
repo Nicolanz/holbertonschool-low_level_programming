b1843b289c5ea9af682f5596b0d7f79edfc272e1
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-21 21:29:14 +0000

    Seven

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * cap_string - Capitalizes strings.
+ * @s: Variable which holds the string.
+ *
+ * Return: Always 0 (Success)
+ */
+char *leet(char *s)
+{
+	char upper[1000] = {'A', 'E', 'O', 'T', 'L'};
+	char lower[1000] = {'a', 'e', 'o', 't', 'l'};
+	char array[1000] = {'4', '3', '0', '7', '1'};
+	int i;
+
+	for (i = 0; s[i] != '\0', i++)
+	{
+		if ((s[i] == ))
+   	}
+}
