e4704031e76a4f32e8c8c780d88b71042046ddfe
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-22 04:43:33 +0000

    13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * rot13 - Function to encode in rot13.
+ * @s: Variable which holds the string.
+ *
+ * Return: Always 0 (Success)
+ */
+char *rot13(char *s)
+{
+	char abc[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	char mno[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+	int i;
+	int c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (c = 0; (abc[c] != '\0') && (mno[c] != '\0'); c++)
+		{
+			if (s[i] == abc[c])
+			{
+				abc[c] = mno[c];
+				s[i] = abc[c];
+				abc[c] = mno[c + 13];
+				break;
+			}
+		}
+	}
+	return (s);
+}
