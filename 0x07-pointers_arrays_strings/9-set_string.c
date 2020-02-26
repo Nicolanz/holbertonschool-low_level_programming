2a97a69d6ee91b6046d8ea1c84c9e6ca0ea938f6
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-26 04:35:09 +0000

    Set String

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_chessboard - Function that prints the chessboard.
- * @a: Variable which holds the 2D array.
- *
+ * set_string - Function that sets the value of a pointer to a char.
+ * @s: Variable which will change.
+ * @to: Variable which changes the s variable
  * Return: Always 0 (Success)
  */
 void set_string(char **s, char *to)
