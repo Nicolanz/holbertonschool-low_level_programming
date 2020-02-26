f8e5b9bdf4441511289ac277b7d1c1fb459eee8e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-26 04:31:54 +0000

    Nine

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_chessboard - Function that prints the chessboard.
+ * @a: Variable which holds the 2D array.
+ *
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
