3a59a6ac22b1e5af51d295fd08ec147f2f474df3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-27 22:26:14 +0000

    Seis

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - Function to get the pow if a number.
+ * @x: Frist Number.
+ * @y: Second Number.
+ * Return: Always 0 (Sucess)
+ */
+int primo(int i, int n)
+{
+	if (i / i == )
+}
+int is_prime_number(int n)
+{
+	int i = 1;
+}
