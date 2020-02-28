106b841c4bbc1405c2477a3cd01d80438aaba5b4
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-02-28 18:48:56 +0000

    Prime Numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,32 @@
 #include "holberton.h"
 /**
- * _pow_recursion - Function to get the pow if a number.
- * @x: Frist Number.
- * @y: Second Number.
- * Return: Always 0 (Sucess)
+ * primo - Function to identify prime numbers.
+ * @c: Value.
+ * @i: iterator.
+ * Return: Always 1 prime.
  */
-int primo(int i, int n)
+int primo(int c, int i)
 {
-	if (i / i == )
+	if (i <= 1)
+	{
+		return (1);
+	}
+	else if (c % i == 0)
+	{
+		return (0);
+	}
+	return (primo(c, i - 1));
 }
+/**
+ * is_prime_number - Funcion to return if a number is prime.
+ * @n: Value.
+ * Return: Alwais 1 if it is prime
+*/
 int is_prime_number(int n)
 {
-	int i = 1;
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (primo(n, n - 1));
 }
