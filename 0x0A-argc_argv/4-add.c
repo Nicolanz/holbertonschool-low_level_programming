5d75e467a8926d1abfe18799349af657da242cb9
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-02 21:58:11 +0000

    Four

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @argc: Number of arguments in the command line
+ * @argv: String of arguments in the command line
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int c = 0;
+	int b;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (b = 0; argv[i][b]; b++)
+		{
+			if (!(argv[i][b] <= '9' && argv[i][b] >= '0'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		c += atoi(argv[i]);
+	}
+	printf("%d\n", c);
+	return (0);
+}
