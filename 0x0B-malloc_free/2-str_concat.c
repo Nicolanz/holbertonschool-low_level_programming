beaa53c0e054d42bff462bb0af7db09026434c83
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-03 22:50:55 +0000

    two

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,34 +10,31 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i;
-	unsigned int b;
+	int i;
+	int b;
 	char *s;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; s1 && s1[i]; i++)
 	{
 	}
 	for (b = 0; s2 && s2[b]; b++)
 	{
 	}
-	s = malloc((i + b) * sizeof(char));
+	s = malloc((i + b + 1) * sizeof(char));
 	if (s1 == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] && s1[i]; i++)
+	for (i = 0; s1 && s1[i]; i++)
 	{
 		s[i] = s1[i];
 	}
 	for (b = 0; s2 && s2[b]; b++)
 	{
-		s[i] += s2[b];
+		s[i] = s2[b];
 		i++;
 	}
+	s[i] = '\0';
 	return (s);
 
 }
