a8dc29d89e565079a61caf3c006f3eb2947d7c5f
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-03 23:42:39 +0000

    tres

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+*/
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+
+	array = malloc(height * sizeof(int));
+
+	for(i = 0; i < height; i++)
+	{
+		
+	}
+
+}
