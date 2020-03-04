9ac52370f0b1d00ad573c27878aa333a2f996af4
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-04 22:02:12 +0000

    Three

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,12 +12,29 @@ int **alloc_grid(int width, int height)
 {
 	int **array;
 	int i;
+	int b;
 
-	array = malloc(height * sizeof(int));
-
-	for(i = 0; i < height; i++)
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
 	{
-		
+		return (NULL);
 	}
-
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			for (i = 0; i < width; i++)
+			{
+				free(array[i]);
+			}
+			free(array);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+		{
+			array[i][b] = 0;
+		}
+	}
+	return (array);
 }
