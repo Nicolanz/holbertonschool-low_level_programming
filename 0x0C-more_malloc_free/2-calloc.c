1345f77a919b29eae29e181b2e057a8d25b20342
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-05 21:34:34 +0000

    Point 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - Function to emulate calloc function
+ * @nmemb: Number of elements.
+ * @size: Size of array.
+ * Return: array
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(nmemb * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; array[i]; i++)
+	{
+		array[i] = 0;
+	}
+	return (array);
+}
