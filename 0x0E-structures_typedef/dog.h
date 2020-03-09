5b5ff807f75eff10597029865660f99b03a109a5
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-09 21:39:59 +0000

    Exc 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _SAMPLE_HEADER_H
+#define _SAMPLE_HEADER_H
+
+int _putchar(char c);
+/**
+ * struct dog - Struct about dogs
+ * @name: Name of the dog.
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _SAMPLE_HEADER_H_ */
