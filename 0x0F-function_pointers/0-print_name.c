a804ac6855e5fac75daa69fe4d398fe743fbc2f3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-11 20:20:11 +0000

    Point 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - Print name
  * @name: Name to print
@@ -7,5 +8,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+	{
+		f(name);
+	}
 }
