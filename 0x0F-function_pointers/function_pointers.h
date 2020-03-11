8a909841066d9f41912ccc797d283bbe003eb1c2
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-11 22:49:38 +0000

    Two

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
-typedef __SIZE_TYPE__ size_t;
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* _SAMPLE_HEADER_H_ */
