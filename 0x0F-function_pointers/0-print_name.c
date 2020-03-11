456e4bd6b7b79dd5b7ef2dabee24427e3ac0aac4
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-11 20:12:21 +0000

    One

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Print name
+ * @name: Name to print
+ * @f: Pointer function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif /* _SAMPLE_HEADER_H_ */
