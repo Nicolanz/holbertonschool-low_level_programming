0c1328d24e4f4de9df411097ee050e92ddbdd81f
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-12 04:03:24 +0000

    Two

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,25 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * int_index - Searches for intergers
+ * @cmp: Pointer to function
+ * @array: Array to iterate
+ * @size: Size of the array
+ * Return: index(i)
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 	int b = 0;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
 	for (i = 0; i < size; i++)
 	{
 		b = cmp(array[i]);
-		if(b != 0)
+		if (b != 0)
 		{
 			break;
 		}
