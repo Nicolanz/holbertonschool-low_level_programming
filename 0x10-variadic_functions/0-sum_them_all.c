fcd09f3c7867ca82842279b206327984267122ce
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-13 21:29:17 +0000

    10

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,6 +12,11 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i = 0, sum  = 0;
 	va_list arguments;
 
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	va_start(arguments, n);
 
 	for (i = 0; i < n; i++)
