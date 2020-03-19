8a7ccf6c727cc0699f89fef6f218c1a662ad5d05
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-19 23:11:48 +0000

    Cero

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,25 @@ size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	if (h->str == NULL)
+	if (h != NULL)
 	{
-		printf("[0] (nil)");
-		printf("\n");
-		h = h->next;
-		count++;
-	}
-	while (h != NULL)
-	{
-		printf("[%d] %s", h->len, h->str);
-		printf("\n");
-		h = h->next;
-		count++;
+		while (h != NULL)
+		{
+			if (h->str == NULL)
+			{
+				printf("[0] (nil)");
+				printf("\n");
+				h = h->next;
+				count++;
+			}
+			else
+			{
+				printf("[%d] %s", h->len, h->str);
+				printf("\n");
+				h = h->next;
+				count++;
+			}
+		}
 	}
 	return (count);
 }
