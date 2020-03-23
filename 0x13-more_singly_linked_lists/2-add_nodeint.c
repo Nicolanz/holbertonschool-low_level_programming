d87f3d38f5cff9ead6a8bd32770336153b65a481
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-23 19:12:49 +0000

    2 Exc

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_nodeint - Adds new node.
+ * @head: head
+ * @n: Number
+ * Return: newnode
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -21,5 +21,6 @@ typedef struct listint_s
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /*HOLBERTON*/
