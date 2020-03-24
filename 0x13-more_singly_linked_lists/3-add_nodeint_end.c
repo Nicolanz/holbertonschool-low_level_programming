d543159c70ba0cb4e15c0f14feb3ed2e09aa0d65
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-03-24 14:57:07 +0000

    Exc 3 & 4

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Adds node at the end.
+ * @n: Number
+ * @head: head;
+ * Return: newnode;
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newnode;
+	listint_t *last = *head;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (*head);
+	}
+	newnode->n = n;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = newnode;
+	return (newnode);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - Frees list
+ * @head: head;
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while(temp != NULL)
+	{
+		free(temp);
+		temp = temp->next;
+	}
+}
