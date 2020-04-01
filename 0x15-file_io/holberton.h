6a3b348952b034d09c9bef56f3c897d06de827c6
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-04-01 02:43:00 +0000

    Holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,14 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 
-int _putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#enddif/*HOLBERTON*/
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif/*HOLBERTON*/
