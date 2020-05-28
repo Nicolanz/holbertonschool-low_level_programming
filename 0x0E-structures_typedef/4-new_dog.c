f9ca697aff5d5529cbc132ee46a2de98449e9aaa
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-28 03:26:24 +0000

    Number Four

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,15 +9,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *nombre;
+	char *pro;
+	int i;
 
+	for (i = 0; name[i] != '\0'; i++)
+	{}
+	i++;
+	nombre = malloc(i * sizeof(char));
+	if (nombre == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		nombre[i] = name[i];
+	}
+	nombre[i] = '\0';
+
+	for (i = 0; owner[i] != '\0'; i++)
+	{}
+	i++;
+	pro = malloc(i * sizeof(char));
+	if (pro == NULL)
+		return (NULL);
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		pro[i] = owner[i];
+	}
+	pro[i] = '\0';
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->name = name;
+	dog->name = nombre;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = pro;
 	return (dog);
 }
