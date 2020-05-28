dd37abdadf91a39654acffa4df409a9cee2a4678
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-28 03:51:08 +0000

    #4oo

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,6 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	i++;
 	nombre = malloc(i * sizeof(char));
 	if (nombre == NULL)
+		return(NULL);
 		free(nombre);
 	for (i = 0; name[i] != '\0'; i++)
 	{
@@ -30,6 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	i++;
 	pro = malloc(i * sizeof(char));
 	if (pro == NULL)
+		return(NULL);
 		free(pro);
 	for (i = 0; owner[i] != '\0'; i++)
 	{
