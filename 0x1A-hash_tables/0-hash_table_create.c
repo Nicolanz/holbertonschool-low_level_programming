16bc882a5b4aaeb49b755715076cd912b7d22f42
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-06-05 00:30:17 +0000

    New project

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -
+ *
+ * Return: 
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ptr_to_table = NULL;
+    hash_node_t **temp = NULL;
+    int i;
+
+    ptr_to_table = malloc(sizeof(hash_table_t));
+    if (ptr_to_table == NULL)
+    {
+        return (NULL);
+    }
+    
+    temp = malloc(sizeof(hash_node_t *) * size);
+    if (temp == NULL)
+    {
+        return (NULL);
+    }
+    ptr_to_table->size = size;
+
+    for (i = 0; i < size; i++)
+    {
+        temp[i] = NULL;
+    }
+
+    ptr_to_table->array = temp;
+
+    return(ptr_to_table);
+}
