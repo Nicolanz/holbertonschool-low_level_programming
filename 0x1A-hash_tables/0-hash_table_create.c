fb240b633aab2c9a1f34540e5aab9f41ce4f194d
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-06-05 01:09:38 +0000

    Task #0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,33 +4,32 @@
  * hash_table_create - Function to create a new hash table
  * @size: size of the hash table
  * Return: Pointer to the hash table
- * 
+ *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ptr_to_table = NULL;
-    hash_node_t **temp = NULL;
-    int i;
+	hash_table_t *ptr_to_table = NULL;
+	hash_node_t **temp = NULL;
+	int i;
 
-    ptr_to_table = malloc(sizeof(hash_table_t));
-    if (ptr_to_table == NULL)
-    {
-        return (NULL);
-    }
-    
-    temp = malloc(sizeof(hash_node_t *) * size);
-    if (temp == NULL)
-    {
-        return (NULL);
-    }
-    ptr_to_table->size = size;
+	ptr_to_table = malloc(sizeof(hash_table_t));
+	if (ptr_to_table == NULL)
+	{
+		return (NULL);
+	}
+	temp = malloc(sizeof(hash_node_t *) * size);
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	ptr_to_table->size = size;
 
-    for (i = 0; i < size; i++)
-    {
-        temp[i] = NULL;
-    }
+	for (i = 0; i < size; i++)
+	{
+		temp[i] = NULL;
+	}
 
-    ptr_to_table->array = temp;
+	ptr_to_table->array = temp;
 
-    return(ptr_to_table);
+	return (ptr_to_table);
 }
